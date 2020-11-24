4b6866a7e333cdac728835a1fd33dc12106dcc52
papaiz-debora <73493656+papaiz-debora@users.noreply.github.com>
2020-11-24 12:00:44 -0300

    Update models.py

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
   author = models.ForeignKey(User, on_delete=models.PROTECT)
   micro_organism = models.CharField(max_lenght=300)
   content= models.TextField()
-  created_at = models.DataField()
-  altered_at = models.DataField(auto_now_add=True)
+  created_at = models.DataField(auto_now_add=True)
+  altered_at = models.DataField()
 
 def __str__(self):
         return self.micro_organism
